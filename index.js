ea161a56f117162b021fa784cb4bc5c457d4ab1b
Thiago Santos <thiagosantosdev@gmail.com>
2021-12-08 12:53:26 -0300

    another ways to create a new Circle

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-// Factory function
-
-function createCircle(radius) {
-  return {
-    radius,
-      draw: function () {
-        console.log('draw');
-      }
-  };
-}
-
-const circle = createCircle(1);
-
 // Constructor function
-function Circle(radius){
+function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
     console.log('draw');
   }
 }
+
+const Circle1 = new Function('radius', `
+    this.radius = radius;
+    this.draw = function () {
+      console.log('draw');
+    }
+`)
+
+const circle = new Circle1(1);
 
 const another = new Circle(1);
