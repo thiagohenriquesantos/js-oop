57530ea46695f28bece739d0a5d6567bb6e6bdaf
Thiago Santos <thiagosantosdev@gmail.com>
2021-12-08 12:53:55 -0300

    another ways to create a new Circle

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ function Circle(radius) {
   }
 }
 
-const Circle1 = new Function('radius', `
-    this.radius = radius;
-    this.draw = function () {
-      console.log('draw');
-    }
-`)
-
-const circle = new Circle1(1);
+Circle.call({}, 1)
+Circle.apply({}, [1, 2, 3])
 
 const another = new Circle(1);
